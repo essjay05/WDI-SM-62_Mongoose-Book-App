ba450af0e2bb5f09083f0c77b9ff7a3e38508841
Joy <stepjoyous@gmail.com>
2018-12-06 16:41:25 -0800

    Completed/created Book.js in Models folder... ready for export/use

--- models/book.js
+++ models/book.js
@@ -1,1 +1,20 @@
 // Require mongoose in here.
+
+// book.js
+const 
+    mongoose = require ('mongoose'),
+    Schema = mongoose.Schema;
+
+    // CREATE Book Schema for BOOK COLLECTION
+const BookSchema = new Schema({
+        title: String,
+        author: String,
+        image: String,
+        releaseDate: Date
+    }, {timestamps: true});
+
+// MAKE THIS A MODEL
+const Book = mongoose.model('Book', BookSchema);
+
+// MAKE AVAILABLE FOR EXPORT
+module.exports = Book;
