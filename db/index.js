dda2c093a3582fa9aaeca9519fac5c4b37bd75fd
Joy <stepjoyous@gmail.com>
2018-12-06 17:15:46 -0800

    Successfully set up mLab database and connected to mongoose-book-app server :)

--- db/index.js
+++ db/index.js
@@ -1,4 +1,8 @@
 // Mongoose setup goes here.
 const mongoose = require('mongoose');
 
-mongoose.connect()
+mongoose.connect(process.env.MONGOD_URI, { useNewUrlParser: true }, err => {
+    console.log(err || `Successfully connected to mLab: Mongoose-Book-App`)
+});
+
+module.exports = mongoose;
